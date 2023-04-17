74f424892ad9a958e2f58f1f419db956b181b3ec
Kendall Holland <holland.kd@gmail.com>
2023-04-17 19:59:33 -0400

    Finishing easy problems 2

--- lesson_3/easy_1.rb
+++ lesson_3/easy_1.rb
@@ -10,9 +10,27 @@
 
 
 # lol OR
-advice = "Few things in life are as important as house training your pet dinosaur."
+# advice = "Few things in life are as important as house training your pet dinosaur."
 
-advice.gsub!('important', 'urgent')
+# advice.gsub!('important', 'urgent')
 
-puts advice
+# puts advice
+
+# puts (10...100).include?(42)
+
+famous_words = "seven years ago..."
+
+# puts "Four score and " << famous_words
+# famous_words = "Four score and " + famous_words
+
+# flintstones = ["Fred", "Wilma"]
+# flintstones << ["Barney", "Betty"]
+# flintstones << ["BamBam", "Pebbles"]
+
+# p flintstones.flatten!
+
+flintstones = { "Fred" => 0, "Wilma" => 1, "Barney" => 2, "Betty" => 3, "BamBam" => 4, "Pebbles" => 5 }
+
+barney = flintstones.select { |x,v| x == "Barney"}
+p barney.to_a.flatten!
 
---
+++ lesson_3/easy_2.rb
@@ -0,0 +1,47 @@
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 402, "Eddie" => 10 }
+
+# puts ages.any?("Spot")
+# puts ages.has_key?("Spot")
+# puts ages.include?("Spot")
+
+munsters_description = "The Munsters are creepy in a good way."
+
+# puts munsters_description.swapcase
+# puts munsters_description.capitalize
+# puts munsters_description.downcase
+# puts munsters_description.upcase
+
+# ages = { "Herman" => 32, "Lily" => 30, "Grandpa" => 5843, "Eddie" => 10 }
+# additional_ages = { "Marilyn" => 22, "Spot" => 237 }
+
+# ages.merge!(additional_ages)
+
+# p ages
+
+# advice = "Few things in life are as important as house training your pet dinosaur."
+
+# puts advice.match?("Dino")
+
+# flintstones = ["Fred", "Barney", "Wilma", "Betty", "BamBam", "Pebbles"]
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles)
+
+
+
+# flintstones = %w(Fred Barney Wilma Betty BamBam Pebbles) 
+
+# flintstones.push("Dino", "Hoppy")
+
+# puts flintstones
+
+# advice = "Few things in life are as important as house training your pet dinosaur."
+
+# p advice.slice!(0,36) --- I could have also done 0, advice.index('house')
+# p advice
+
+# statement = "The Flintstones Rock!"
+
+# p statement.count("t")
+
+title = "Flintstone Family Members"
+
+p title.center(600)
